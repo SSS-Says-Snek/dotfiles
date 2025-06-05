eabdb4e67e3e162d322f9ff0ddd19d5d5bd70c1a
Brandon <bmcomis2018@gmail.com>
2025-06-04 20:22:08 -0500

    Gammastep

--- nvim/lua/bdon/plugins/lsp/mason.lua
+++ nvim/lua/bdon/plugins/lsp/mason.lua
@@ -5,22 +5,4 @@ return {
       require("mason").setup()
     end
   },
-
-  {
-    'williamboman/mason-lspconfig.nvim',
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      'williamboman/mason.nvim'
-    },
-
-    config = function()
-      local mason_lspconfig = require("mason-lspconfig")
-      mason_lspconfig.setup({
-        ensure_installed = {
-        },
-        automatic_installation = true
-      })
-    end,
-    lazy = true
-  }
 }
